2cf037286d5b1e9ebbd102275102f9ce386a4b33
Nuclear Squid <leo@cazenave.cc>
2026-08-29 23:18:56 +0200

    apply settings

--- include/aekeynox/settings.h
+++ include/aekeynox/settings.h
@@ -10,7 +10,7 @@
 // a non-QWERTY-ANSI keyboard layout. This is required to set an appropriate
 // 'Symbols' layer and relevant keyboard shortcuts in the 'Nav' layer.
 
-// #define KB_LAYOUT_AZERTY            // France
+#define KB_LAYOUT_AZERTY            // France
 // #define KB_LAYOUT_BEPO              // France
 // #define KB_LAYOUT_BEPOLAR           // France
 // #define KB_LAYOUT_DVORAK            // US
